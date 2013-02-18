01288b13e09ae07d420bda3164ca2c6a7b1f668a
j0hnnyF1ve <john.pangilinan1@gmail.com>
2013-02-18 18:15:42 +0800

    Resume commit

---
+++ resume/accordion.php
@@ -0,0 +1,44 @@
+<div id="Accordion">
+
+    <h2><a href="#">Objective</a></h2>
+    <div id="Accordion_Objective">
+    <?php
+      include('content/background.html');      
+    ?>      
+    </div>
+    <h2><a href="#">Education</a></h2>
+    <div id="Accordion_Education">
+    <?php
+      include('content/education.html');      
+    ?>      
+    </div>
+
+    <h2><a href="#">Skills</a></h2>
+    <div id="Accordion_Skills">
+    <?php
+      include('content/skills.html');      
+    ?>      
+    </div>
+
+    <h2><a href="#">Current Interests</a></h2>
+    <div id="Accordion_CurrentInterests">
+    <?php
+      include('content/currentInterests.html');      
+    ?>      
+    </div>
+
+    <h2><a href="#">Work Experience</a></h2>
+    <div id="Accordion_WorkExperience">
+    <?php
+      include('content/workExperience.html');      
+    ?>      
+    </div>
+
+    <h2><a href="#">Other Skills</a></h2>
+    <div id="Accordion_OtherSkills">
+    <?php
+      include('content/otherSkills.html');      
+    ?>      
+    </div>
+
+</div>
